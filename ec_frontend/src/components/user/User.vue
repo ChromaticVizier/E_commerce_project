f44087ee74a543fd0182364b143b1027e3a8c03d
Chromatic Vizier <Zjq8947@gmail.com>
2024-04-17 15:11:37 +0800

    Added crumbs to the user interface (top navigation).

--- ec_frontend/src/components/user/User.vue
+++ ec_frontend/src/components/user/User.vue
@@ -3,6 +3,7 @@
 </script>
 
 <template>
+  <!-- 面包屑 -->
   <el-breadcrumb separator-class="el-icon-arrow-right">
     <el-breadcrumb-item :to="{ path: '/home' }">Homepage</el-breadcrumb-item>
     <el-breadcrumb-item>User management</el-breadcrumb-item>
